36b4dcf904b0b826e72419fe1ce3bdc05a0e3745
luojinfu <luojinfu7@foxmail.com>
2016-09-21 23:30:34 +0800

    service impl

---
+++ src/main/java/org/seckill/exception/SeckillExecption.java
@@ -0,0 +1,7 @@
+package org.seckill.exception;
+
+/**
+ * Created by jinfu on 2016/9/21.
+ */
+public class SeckillExecption {
+}
---
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -0,0 +1,7 @@
+package org.seckill.service.impl;
+
+/**
+ * Created by jinfu on 2016/9/21.
+ */
+public class SeckillServiceImpl {
+}
